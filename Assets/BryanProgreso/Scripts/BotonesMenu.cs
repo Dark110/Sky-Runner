f4591b5bf6631b9f52b29711a9a1d365a7e0d1af
zap626-stack <31224002+Dark110@users.noreply.github.com>
2025-09-01 20:36:16 -0600

    Menu Corregido Completo

--- Assets/BryanProgreso/Scripts/BotonesMenu.cs
+++ Assets/BryanProgreso/Scripts/BotonesMenu.cs
@@ -3,13 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public void JugarMenu()
-    {
-     
-        SceneManager.LoadScene("JugarMenu");
-    }
-
-
+    
     public void Jugar()
     {
 
