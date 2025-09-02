50bd866617858f12ae987e41d6df7a2e7f775364
turambar15 <111933584+Pepe2505@users.noreply.github.com>
2025-09-01 21:04:57 -0600

    Movimiento jugador
    Ya se mueve Izquierda y Derecha

---
+++ Assets/PepeProgreso/Scripts/MovimientoJugador.cs
@@ -0,0 +1,45 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class MovimientoJugador : MonoBehaviour
+{
+    public float Vel = 50f;
+    public bool ControlSuavidad = true;
+    public float NivelSuavidad = 1f;
+    float IzqDer = 50f;
+
+    private Vector3 JugadorMov;
+
+    private bool ladoIzquierda = false;
+    private bool ladoDerecho = false;
+
+    private void Update()
+    {
+
+#if UNITY_EDITOR
+        //detecta el movimiento del celular de Izquierda y Derecha
+        IzqDer = Input.GetAxis("Horizontal");
+
+        Debug.Log(IzqDer);
+#else
+ //detecta el movimiento del celular de Izquierda y Derecha
+        IzqDer = Input.acceleration.x;
+#endif
+
+        //Guarda el movimento en su actualizacion
+        JugadorMov = new Vector3(IzqDer, 0, 0);
+        transform.Translate(JugadorMov * Time.deltaTime);
+
+        // el if y else para que el jugador escoga que lado
+    }
+
+    public void izquierda(bool lado)
+    {
+        ladoIzquierda = lado;
+    }
+
+    public void derecha(bool lado)
+    {
+        ladoDerecho = lado;
+    }
+}
