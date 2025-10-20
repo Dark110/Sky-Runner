2fa36f2d48eb585d1d36d3621d7d04d01ee93ff6
Pepe2505 <111933584+Pepe2505@users.noreply.github.com>
2025-10-20 14:44:44 -0600

    Controlador de volmen
    Ya funciona el volumen y los efectos

--- Assets/BryanProgreso/Scripts/Scripts Menu/VolumenEfectos.cs
+++ Assets/BryanProgreso/Scripts/Scripts Menu/VolumenEfectos.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class VolumenEfectos : MonoBehaviour
 {
+    [Header("Mixer y Slider")]
+    public AudioMixer AudioMix;
+    public Slider Volumen;
+    public Slider Efectos;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //cargar los volumens de Guardadado
+        float MusicVol = PlayerPrefs.GetFloat("VolumenMusica", 0.75f);
+        float EfectoVol = PlayerPrefs.GetFloat("SFXVolumen", 0.75f);
+
+        Volumen.value = MusicVol;
+        Efectos.value = EfectoVol;
+
+        SetMusicVolumen(MusicVol);
+        SetSFXVolumen(EfectoVol);
+
+        //Se escuchan los cambios de volumen
+        Volumen.onValueChanged.AddListener(SetMusicVolumen);
+        Efectos.onValueChanged.AddListener(SetSFXVolumen);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetMusicVolumen(float value)
     {
-        
+        AudioMix.SetFloat("MusicVolumen", Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat("MusicVolumen", value);
+    }
+
+    public void SetSFXVolumen(float value)
+    {
+        AudioMix.SetFloat("SFXVolumen", Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat("SFXVolumen", value);
     }
 }
